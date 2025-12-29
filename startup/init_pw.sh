c0cb8ad733ac4fea02f9cab42b90f1c1b9fa4e51
Helen <helen_yrofeeva@epam.com>
2025-12-29 15:13:37 +0300

    [EPMHLM-466] nginx - set X-Forwarded-Host for spring actuator links

--- startup/init_pw.sh
+++ startup/init_pw.sh
@@ -183,6 +183,8 @@ server_name localhost;
             proxy_set_header X-Real-IP $remote_addr;
             proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
             proxy_set_header X-Forwarded-Proto $scheme;
+            proxy_set_header X-Forwarded-Host $host;
+            proxy_set_header X-Forwarded-Port $server_port;
             
             # WebSocket support
             proxy_http_version 1.1;
